6583af94b688611affaa34d0a5d97494585ce474
WisdomNwaiwu <wisdom.nwaiwu.peer@gmail.com>
2025-04-09 12:22:31 +0200

    Eslint for linting

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: true,
+          tabWidth: 2,
+          trailingComma: 'es5',
+        },
+      ],
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+];
